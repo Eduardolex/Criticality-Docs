f229b4b9dc78566b4f29971934903bbce6a1a7e5
Eduardolex <ntomer010@gmail.com>
2026-08-05 22:18:45 -0700

    fix footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,4 +1,5 @@
 import React, {type ReactNode} from 'react';
+import {useLocation} from '@docusaurus/router';
 
 import {useThemeConfig} from '@docusaurus/theme-common';
 import FooterLinks from '@theme/Footer/Links';
@@ -7,10 +8,13 @@ import FooterCopyright from '@theme/Footer/Copyright';
 import FooterLayout from '@theme/Footer/Layout';
 
 function Footer(): ReactNode {
+  const {pathname} = useLocation();
   const {footer} = useThemeConfig();
-  if (!footer) {
+
+  if (!footer || pathname === '/docs' || pathname.startsWith('/docs/')) {
     return null;
   }
+
   const {copyright, links, logo, style} = footer;
 
   return (
